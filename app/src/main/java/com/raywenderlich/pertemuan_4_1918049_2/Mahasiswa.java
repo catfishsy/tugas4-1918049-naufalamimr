ffa7808c6f31489c48b73c94cb5c4307382021a9
amima <ninja_cat712@yahoo.com>
2021-10-27 09:19:26 +0700

    Update Tugas 4 1918049

--- app/src/main/java/com/raywenderlich/pertemuan_4_1918049_2/Mahasiswa.java
+++ app/src/main/java/com/raywenderlich/pertemuan_4_1918049_2/Mahasiswa.java
@@ -2,28 +2,43 @@ package com.raywenderlich.pertemuan_4_1918049_2;
 
 public class Mahasiswa {
     private String nama,nim,angkatan;
-    public Mahasiswa(String nama, String nim, String angkatan){
+    private int images;
+    public Mahasiswa(String nama, String nim, String angkatan,int images){
         this.nama = nama;
         this.nim = nim;
         this.angkatan = angkatan;
+        this.images = images;
     }
     public String getNama(){
         return nama;
     }
-    public void setNama(String nama){
+    public void setNama(String nama)    {
+
         this.nama = nama;
     }
     public String getNim(){
+
         return nim;
     }
     public void setNim(String nim){
+
         this.nim = nim;
     }
     public String getAngkatan(){
+
         return angkatan;
     }
     public void setAngkatan(String angkatan){
+
         this.angkatan = angkatan;
+    }
+    public int getimages(){
+
+        return images;
+    }
+    public void setimages(int images){
+
+        this.images = images;
     }
 }
 
--- app/src/main/java/com/raywenderlich/pertemuan_4_1918049_2/MainActivity.java
+++ app/src/main/java/com/raywenderlich/pertemuan_4_1918049_2/MainActivity.java
@@ -12,26 +12,25 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private MahasiswaAdapter adapter;
     private ArrayList<Mahasiswa> mahasiswaArrayList;
+    String[] Name = {"Red Eyes Black Dragon","Blue Eyes Shinning Dragon","Dark Magician","Lord of The Red","Palladium Oracle Mahad","Silent Magician"};
+    String[] type = {"Dragon","Dragon","Spellcaster","Dragon","Spellcaster","Spellcaster"};
+    String[] attribute = {"Dark","Light","Dark","Dark","Light","Light"};
+    int[] images = {R.drawable.redeyes,R.drawable.blueshinnind,R.drawable.darkmagician,R.drawable.lordofthered,R.drawable.mahad,R.drawable.silentmagician};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addData();
-        recyclerView =
-                (RecyclerView)findViewById(R.id.recycleview);
+        recyclerView = (RecyclerView)findViewById(R.id.recycleview);
         adapter = new MahasiswaAdapter(mahasiswaArrayList);
-        RecyclerView.LayoutManager layoutManager = new
-                LinearLayoutManager(MainActivity.this);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
     }
     private void addData() {
         mahasiswaArrayList = new ArrayList<>();
-        mahasiswaArrayList.add(new Mahasiswa("Agung", "1218019",
-                "2012"));
-        mahasiswaArrayList.add(new Mahasiswa("Abi", "1118002",
-                "2011"));
-        mahasiswaArrayList.add(new Mahasiswa("Yudis", "1118031",
-                "2011"));
+        for (int i = 0;i<Name.length;i++) {
+            mahasiswaArrayList.add(new Mahasiswa(Name[i],type[i],attribute[i],images[i]));
+        }
     }
 }
